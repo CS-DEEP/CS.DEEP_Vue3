2512e87f2bf9931bb98f092f48c57f5f624e73a9
2aurora2 <1971261337@qq.com>
2023-07-15 15:49:08 +0800

    NavBar

--- src/views/user/UserInfoView.vue
+++ src/views/user/UserInfoView.vue
@@ -1,19 +1,14 @@
 <template>
-  <navTop/>
   <div class="user-page">
     <span>此用户userId为: {{ userId }}</span>
   </div>
 </template>
 
 <script lang="ts">
-import navTop from '../../components/common/navTop.vue'
 export default {
   name: "UserInfoView",
   created() {
     this.userId = this.$route.params.userId;
-  },
-  components:{
-    navTop
   },
   data() {
     return {
