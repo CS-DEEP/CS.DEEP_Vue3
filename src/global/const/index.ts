05e16e9969927f364a87e4d6ae3a6ae974682589
2aurora2 <1971261337@qq.com>
2023-08-04 23:02:18 +0800

    一级评论展示及回复

--- src/global/const/index.ts
+++ src/global/const/index.ts
@@ -2,6 +2,7 @@
 *   全局常量的定义
 *   创建时间: 2023-07-09
 * */
+
 const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 const COLORS = ['#ebedf0', '#c6e48b', '#7bc96f', '#239a3b', '#196127']
 const DEFAULTUSERINFO = {
@@ -21,31 +22,7 @@ const DEFAULTARTICLE = {
     id: 0,
     authorId: 0,
     title: '这是一篇测试文章',
-    content: `
-## 前言
-基于命令行界面的C++程序设计的小游戏——猜数字
-
->测试：
->
->* 点赞数和收藏数正常显示+超过1000数会不会转换为类似'1.2k'
-
-| 姓名 | 成绩 | 等级 |
-| ---- | ---- | ---- |
-| Alen | 60   | A    |
-| Mike | 52   | B    |
-| Jane | 22   | C    |
-
-\`\`\`C++
-#include<iostream>
-using namespace std;
-int main(void){
-    cout<<11<<endl;
-    return 0;
-}
-\`\`\`
-$$
-x+y=z
-$$`,
+    content: ``,
     categoryId: -1,
     createTime: 1689949389,
     updateTime: 1689949389,
@@ -55,15 +32,25 @@ $$`,
 const CATEGORYLIST = ['专业知识', '分享发现', '吐槽讨论']
 const DEFAULTCOMMENT = {
     id: 1,
-    content: "这是一条测试评论",
+    content: "这是一条测试评论😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀勋哥牛哇带带我；保佑明天10连抽不要歪！！！！",
     authorId: 1,
+    articleId: 1,
     createTime: 1690965366840,
     isReply: 0,
     replyId: 1,
     isNasty: 0,
     version: null
 }
-
+const DEFAULTONELEVELCOMMENT = {
+    avatar: 'https://img.51miz.com/Element/00/88/08/84/72f298b9_E880884_d0f63115.png',
+    name: '布林布林的',
+    content: DEFAULTCOMMENT,
+    isShowTwoLevelComment: false,
+    isShowEmoji: false,
+    replyContent: '',
+    publishTime: '2023-8-4 22:20:20',
+    numOfReply: 0
+}
 
 export default {
     MONTHS,          // 各月份的英文简写
@@ -72,4 +59,5 @@ export default {
     DEFAULTARTICLE,  // 默认文章信息
     CATEGORYLIST,    // 文章分类
     DEFAULTCOMMENT,  // 默认评论
+    DEFAULTONELEVELCOMMENT, // 默认一级评论
 }
