b1a8d4c2f605dfc33075d28c36fdd66ea40db54b
2aurora2 <1971261337@qq.com>
2024-03-27 15:30:38 +0800

    雷达图UI

--- src/views/persona/MyPersonaView.vue
+++ src/views/persona/MyPersonaView.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="persona-page">
-    <h2 style="border-left: 3px solid #41b997;border-radius: 3px;padding-left: 5px">维度得分</h2>
-    <div style="margin-top: 10px">
-      <div
-        style="width: 40px;height: 20px;background-color: #5470c6;float: left;border-radius: 5px;margin-right: 5px"></div>
-      区域代表该维度得分
-    </div>
+    <h3 style="border-left: 3px solid #41b997;border-radius: 3px;padding-left: 5px">总体评价得分<span
+      style="font-size: 10px">(蓝色区域为得分区域)</span></h3>
     <div class="score-box">
-      <div id="score-0" style="width: 130px;height: 130px"></div>
-      <div id="score-1" style="width: 130px;height: 130px"></div>
-      <div id="score-2" style="width: 130px;height: 130px"></div>
-      <div id="score-3" style="width: 130px;height: 130px"></div>
+      <div id="score-0" style="width: 200px;height: 200px"></div>
+      <div id="score-1" style="width: 200px;height: 200px"></div>
+      <div id="score-2" style="width: 200px;height: 200px"></div>
+      <div id="score-3" style="width: 200px;height: 200px"></div>
     </div>
-    <h2 style="border-left: 3px solid #41b997;border-radius: 3px;padding-left: 5px;margin-top: 20px">维度建议</h2>
+    <h3 style="border-left: 3px solid #41b997;border-radius: 3px;padding-left: 5px;margin-top: 20px">
+      二级维度详细得分</h3>
+    <div class="sub-score-box">
+      <div id="sub-score" style="width: 800px;height: 530px"></div>
+    </div>
+    <h3 style="border-left: 3px solid #41b997;border-radius: 3px;padding-left: 5px;margin-top: 20px">维度建议</h3>
     <div class="suggestion-box">
-      <h3 style="border-left: 3px solid #b9c2bf;border-radius: 3px;padding-left: 5px;margin-top: 8px">
-        在线自主学习能力:</h3>
+      <h4 style="border-left: 3px solid #b9c2bf;border-radius: 3px;padding-left: 5px;margin-top: 12px">
+        在线自主学习能力:</h4>
       <div>{{ suggestionList[0] }}</div>
-      <h3 style="border-left: 3px solid #b9c2bf;border-radius: 3px;padding-left: 5px;margin-top: 8px">
-        在线协作学习能力:</h3>
+      <h4 style="border-left: 3px solid #b9c2bf;border-radius: 3px;padding-left: 5px;margin-top: 8px">
+        在线协作学习能力:</h4>
       <div>{{ suggestionList[1] }}</div>
-      <h3 style="border-left: 3px solid #b9c2bf;border-radius: 3px;padding-left: 5px;margin-top: 8px">
-        数字化学习能力:</h3>
+      <h4 style="border-left: 3px solid #b9c2bf;border-radius: 3px;padding-left: 5px;margin-top: 8px">
+        数字化学习能力:</h4>
       <div>{{ suggestionList[2] }}</div>
     </div>
   </div>
@@ -33,19 +34,22 @@ import api from '@/api/modules'
 import { ElMessage } from 'element-plus'
 import * as echarts from 'echarts'
 
-const scoreList = ref([])
-const suggestionList = ref([])
+const scoreList = ref([1, 2, 3, 4])
+const suggestionList = ref(['没有建议', '没有建议', '没有建议'])
+const subScore = ref([[1, 2, 3, 4], [0, 2, 1, 4], [2, 3, 4, 1]])
 const dom0 = shallowRef()
 const dom1 = shallowRef()
 const dom2 = shallowRef()
 const dom3 = shallowRef()
+const dom4 = shallowRef()
 
 onMounted(() => {
   dom0.value = echarts.init(document.getElementById('score-0'))
   dom1.value = echarts.init(document.getElementById('score-1'))
   dom2.value = echarts.init(document.getElementById('score-2'))
   dom3.value = echarts.init(document.getElementById('score-3'))
-  // drawScorePie()
+  dom4.value = echarts.init(document.getElementById('sub-score'))
+  drawScorePie()
   // 获取所有结果
   api.personaApi.getOverallResult().then(res => {
     if (res.data.code === 200) {
@@ -194,8 +198,91 @@ const drawScorePie = () => {
           show: false
         },
         data: [
-          { value: 5 - scoreList.value[3], name: '' },
-          { value: scoreList.value[3], name: '' }
+          { value: scoreList.value[3], name: '' },
+          { value: 5 - scoreList.value[3], name: '' }
+        ]
+      }
+    ]
+  })
+  dom4.value.setOption({
+    tooltip: {
+      trigger: 'axis'
+    },
+    radar: [
+      {
+        indicator: [
+          { text: '技术及目标计划制定', max: 5, color: '#000000' },
+          { text: '学习效果评价', max: 5, color: '#000000' },
+          { text: '学习过程调控', max: 5, color: '#000000' },
+          { text: '学习内容及材料利用', max: 5, color: '#000000' }
+        ],
+        center: ['24%', '37%'],
+        radius: 80
+      },
+      {
+        indicator: [
+          { text: '小组积极情绪评价', max: 5, color: '#000000' },
+          { text: '小组积极情绪表达', max: 5, color: '#000000' },
+          { text: '师生互动沟通', max: 5, color: '#000000' },
+          { text: '小组积极情绪回应', max: 5, color: '#000000' }
+        ],
+        radius: 80,
+        center: ['50%', '80%']
+      },
+      {
+        indicator: [
+          { text: '数字化学习意识', max: 5, color: '#000000' },
+          { text: '数字化学习行为', max: 5, color: '#000000' },
+          { text: '数字化学习管理', max: 5, color: '#000000' },
+          { text: '数字化学习评价', max: 5, color: '#000000' }
+        ],
+        center: ['75%', '37%'],
+        radius: 80
+      }
+    ],
+    series: [
+      {
+        type: 'radar',
+        tooltip: {
+          trigger: 'item'
+        },
+        areaStyle: {},
+        data: [
+          {
+            value: subScore.value[0],
+            name: '在线自主学习能力'
+          }
+        ]
+      },
+      {
+        type: 'radar',
+        radarIndex: 1,
+        areaStyle: {},
+        tooltip: {
+          trigger: 'item'
+        },
+        data: [
+          {
+            value: subScore.value[1],
+            name: '在线协作学习能力'
+          }
+        ]
+      },
+      {
+        type: 'radar',
+        radarIndex: 2,
+        areaStyle: {},
+        tooltip: {
+          trigger: 'item'
+        },
+        axisLabel: {
+          color: '#000000'
+        },
+        data: [
+          {
+            name: '数字化学习能力',
+            value: subScore.value[2]
+          }
         ]
       }
     ]
@@ -207,6 +294,7 @@ onBeforeUnmount(() => {
   dom1.value.dispose()
   dom2.value.dispose()
   dom3.value.dispose()
+  dom4.value.dispose()
 })
 </script>
 
@@ -236,9 +324,9 @@ onBeforeUnmount(() => {
 
   .suggestion-box {
     div {
-      padding-bottom: 5px;
-      padding-top: 5px;
-      padding-right: 5px;
+      padding-bottom: 8px;
+      padding-top: 8px;
+      padding-right: 8px;
     }
   }
 }
